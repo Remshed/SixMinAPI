5579b75ad155f1156f3d0c3b3e3092e372bdb238
Remshed <ramsheedpnr786@gmail.com>
2023-03-16 17:38:41 +0530

    First Commit

---
+++ Data/ICommandRepo.cs
@@ -0,0 +1,17 @@
+using SixAPI.Models;
+
+namespace SixMinApi.Data
+{
+    public interface ICommandRepo
+    {
+        Task SaveChanges();
+        Task<Command?> GetCommandById(int id);
+        Task<IEnumerable<Command>> GetAllCommands();
+        Task CreateCommand(Command cmd);
+
+        void DeleteCommand(Command cmd);
+        
+    }
+
+
+}
